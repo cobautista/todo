0c04c590fe957dc61a9c2482cc7531786ba963b2
cobautista <cobautista@gmail.com>
2021-10-12 20:35:29 +0800

    model update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+
+  validates :title, presence: true, uniqueness: {scope: :user}, length: { maximum: 20 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 60 }
+    # belongs_to :user
+  has_many :tasks, dependent: :destroy
+
+  
 end
